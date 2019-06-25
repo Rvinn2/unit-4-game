1450a7b5283a6e24f4d6f16fbb116f9f60ed4fe6
Ryan Hart <ryan@Ryans-MacBook-Pro.local>
2019-06-24 20:20:22 -0400

    create random number Math Floor Math Random

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -5,12 +5,28 @@ var randomResult;
 var lost;
 var win;
 
+
+randomResult = Math.floor(Math.random() * 69) + 30;
+$("#result").html('Random Result: '+ randomResult);
+// console.log(randomResult)
+
 for(var i = 0; i < 4; i++){
     
+
+    var randomCrys = Math.floor(Math.random() * 11) + 1;
+    // console.log(randomCrys);
+    
+    
     var crystals = $("<div>");
-        crystals.attr("class",'crystals');
+        crystals.attr({
+            "class":'crystals',
+            "data-random": randomCrys
+        });
 
     $(".crystal").append(crystals);
 }
 
-
+$(".crystals").on('click', function(){
+    
+    console.log($(this).attr("data-random"));
+})
